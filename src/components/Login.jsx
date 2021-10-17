9816ff26422c1d7c8675670b663b42d26aeb20bc
Dhananjay Singh <dhananjaysr26@gmail.com>
2021-10-17 13:09:29 +0530

    Feat: Created Login And register Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import '../styles/Login.css'
 function Login() {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+<div class="login">
+	<h1>Time is Money!</h1>
+    <form >
+    	<input type="email" name="u" placeholder="Username" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+        <button className="result-btn mrl10">Login</button>
+    </form>
+</div>
     )
 }
 export default Login
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import '../styles/Login.css'
 function Register() {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+<div class="login">
+	<h1>Welcome to Unqueue Family</h1>
+    <form >
+    <input type="text" name="n" placeholder="Company Name" required="required" />
+    <input type="email" name="u" placeholder="Email id" required="required" />
+    <input type="text" name="add" placeholder="Address" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+<select name="cars" id="cars">
+<option value="0">Select Company Type</option>
+  <option value="Clilic">Clilic</option>
+  <option value="Shop">Shop</option>
+  <option value="Hair Saloon">Hair Saloon</option>
+  <option value="Hotel">Hotel</option>
+  <option value="Others">Others</option>
+</select>
+    </form>
+    <button className="result-btn mrl10">Register</button>
+</div>
     )
 }
 
